fab5c9aa197e67a16311f010875e39dc06daa3eb
User <nuska27052006@gmail.com>
2025-01-20 12:18:17 +0600

    adfsdf

---
+++ src/models/Person.java
@@ -0,0 +1,46 @@
+package models;
+public class Person {
+    private static int idCounter = 1;
+    private int id;
+    private String name;
+    private String surname;
+
+    public Person() {
+        this.id = idCounter++;
+    }
+
+    public Person(String name, String surname) {
+        this.id = idCounter++;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPosition() {
+        return "Student";
+    }
+
+    @Override
+    public String toString() {
+        return id + ". " + name + " " + surname;
+    }
+}
